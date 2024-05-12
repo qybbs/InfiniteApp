83558e2de19cdffc230621106c5273a22777e2a6
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-05-12 22:30:04 +0700

    Commit changes

--- app/src/main/java/com/infinitelearning/jkt48app/presentation/ProfileScreen.kt
+++ app/src/main/java/com/infinitelearning/jkt48app/presentation/ProfileScreen.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.infinitelearning.jkt48app.R
+import com.infinitelearning.infiniteapp.R
 
 @Composable
 fun ProfileScreen(
